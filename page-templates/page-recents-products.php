e3fd0a6baed197c2fc6c504dd9c4db51b672f927
jaroslaw-filipiak <info@j-filipiak.pl>
2025-07-03 21:02:01 +0200

    Update button styles in archive-product.php and enhance email link in page-about.php for better design consistency. Refine CSS for pagination and filter buttons to improve user experience.

---
+++ page-templates/page-recents-products.php
@@ -0,0 +1,182 @@
+<?php
+
+/*
+Template Name: Recents Products
+*/
+
+?>
+
+<?php get_header(); ?>
+
+
+<section class="py-12 lg:py-24 overflow-hidden bg-stone-200">
+    <div class="container mx-auto px-4">
+        <div class="flex mb-4 items-center">
+            <svg id="svg_2fcf097058cb7e4ce3e79e8bfd5757d4" width="8" height="8" viewbox="0 0 9 9" fill="none"
+                xmlns="http://www.w3.org/2000/svg"></svg>
+            <span class="inline-block ml-2 text-sm font-medium text-teal-900">Nowości</span>
+        </div>
+        <div class="border-t pt-16">
+            <div class="flex flex-wrap items-center justify-between mb-20 -mx-4">
+                <div class="w-full sm:w-2/3 px-4 mb-10 sm:mb-0">
+                    <div class="sm:max-w-lg xl:max-w-none">
+                        <h1 class="font-heading text-4xl xs:text-6xl">Nowości</h1>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="product-grid mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div class="product-item group" data-page="1">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 1">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Lorem
+                            Ipsum Dolor Sit</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group" data-page="1">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 2">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">
+                            Consectetur Adipiscing Elit</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group" data-page="1">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 3">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Duis Aute
+                            Irure Dolor</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Duis aute irure dolor in reprehenderit in voluptate velit
+                            esse cillum dolore eu fugiat nulla pariatur.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group" data-page="1">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 4">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Excepteur
+                            Sint Occaecat</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group" data-page="1">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 5">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Sed Ut
+                            Perspiciatis Unde</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Sed ut perspiciatis unde omnis iste natus error sit
+                            voluptatem accusantium doloremque laudantium.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group" data-page="1">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 6">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Totam Rem
+                            Aperiam</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Totam rem aperiam, eaque ipsa quae ab illo inventore
+                            veritatis et quasi architecto beatae vitae dicta sunt.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group hidden" data-page="2">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 7">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Nemo Enim
+                            Ipsam</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+                            aut odit aut fugit, sed quia consequuntur magni dolores.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group hidden" data-page="2">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 8">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Neque
+                            Porro Quisquam</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                            amet, consectetur, adipisci velit.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group hidden" data-page="2">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400" alt="Product 9">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Ut Enim
+                            Ad Minima</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Ut enim ad minima veniam, quis nostrum exercitationem
+                            ullam corporis suscipit laboriosam, nisi ut aliquid.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group hidden" data-page="2">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400"
+                        alt="Product 10">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Quis
+                            Autem Vel Eum</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Quis autem vel eum iure reprehenderit qui in ea voluptate
+                            velit esse quam nihil molestiae consequatur.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group hidden" data-page="2">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400"
+                        alt="Product 11">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">At Vero
+                            Eos Et</h4>
+                        <p class="text-gray-700 mb-4 text-sm">At vero eos et accusamus et iusto odio dignissimos ducimus
+                            qui blanditiis praesentium voluptatum deleniti.</p>
+
+                    </div>
+                </a>
+            </div>
+            <div class="product-item group hidden" data-page="2">
+                <a href="#" class="block">
+                    <img class="block w-full h-64 mb-6 object-cover" src="https://placehold.co/400x400"
+                        alt="Product 12">
+                    <div>
+                        <h4 class="text-xl font-medium group-hover:text-teal-600 transition duration-200 mb-3">Et Harum
+                            Quidem Rerum</h4>
+                        <p class="text-gray-700 mb-4 text-sm">Et harum quidem rerum facilis est et expedita distinctio.
+                            Nam libero tempore, cum soluta nobis est eligendi.</p>
+
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<?php get_footer(); ?>
