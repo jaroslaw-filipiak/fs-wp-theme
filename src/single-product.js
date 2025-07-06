28af10b4acb1d0124b7ba8b7fa5bb8be9679b026
jaroslaw-filipiak <info@j-filipiak.pl>
2025-07-06 14:36:34 +0200

    Add GSAP fo single product layout

--- src/single-product.js
+++ src/single-product.js
@@ -1,1 +1,27 @@
 import './single-product.scss';
+
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,ScrollToPlugin);
+
+
+let tl = gsap.timeline({
+	scrollTrigger: {
+		trigger: '.single-product-gallery',
+		pin: '.entry-summary', 
+		start: 'top 20%', 
+		end: '+=500', 
+		scrub: 1, 
+		markers: false,
+	}
+});
+
+
+
+
+
+
