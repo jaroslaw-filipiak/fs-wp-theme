e4d91a4c7325cec2203fd2770ac6bd211b2da869
jaroslaw-filipiak <info@j-filipiak.pl>
2025-07-06 12:32:56 +0200

    Refactor single product layout by updating CSS and SCSS for improved gallery and content section styling. Adjust HTML structure in the product template to enhance image display and ensure consistent alignment of product details.

--- woocommerce/single-product-gallery.php
+++ woocommerce/single-product-gallery.php
@@ -17,16 +17,17 @@ $images_length = count($attachment_ids);
         if ($product && is_object($product)) {
             $attachment_ids = $product->get_gallery_image_ids();
             $thumbnail_id = get_post_thumbnail_id();
+            $i = 0;
             
             if ($attachment_ids) {
                 foreach ($attachment_ids as $attachment_id) {
-                    
+                    $i++;
                     ?>
-        <div class="single-product-gallery-image">
-            <div class="single-product-gallery-image-inner">
-                <img src="<?php echo wp_get_attachment_url($attachment_id); ?>"
+        <div class="single-product-gallery-image single-product-gallery-image-<?php echo $i; ?>">
+           
+                <img  src="<?php echo wp_get_attachment_url($attachment_id); ?>"
                     alt="<?php echo get_post_meta($attachment_id, '_wp_attachment_image_alt', true); ?>">
-            </div>
+           
         </div>
         <?php
                 }
