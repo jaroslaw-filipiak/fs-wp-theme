60eb6a6119ed61b9776caf4861478879aefe20f4
jaroslaw-filipiak <info@j-filipiak.pl>
2025-07-12 13:30:34 +0200

    wp loop for arvhive products + add placeholder default img

--- archive-product.php
+++ archive-product.php
@@ -12,7 +12,7 @@
             <div class="flex flex-wrap items-center justify-between mb-20 -mx-4">
                 <div class="w-full sm:w-2/3 px-4 mb-10 sm:mb-0">
                     <div class="sm:max-w-lg xl:max-w-none">
-                        <h1 class="font-heading text-4xl xs:text-6xl">Sklep</h1>
+                        <h1 class="font-heading text-4xl xs:text-6xl">Produkty</h1>
                     </div>
                 </div>
             </div>
@@ -51,7 +51,7 @@
                 </div>
             </div>
         </div>
-        <div class="product-grid mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div class="product-grid mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
             <?php
             if (have_posts()) :
                 while (have_posts()) :
@@ -59,10 +59,20 @@
                     global $product;
                     ?>
                     <div class="product-item group">
-                        <a href="<?php the_permalink(); ?>" class="block">
+                        <a href="<?php the_permalink(); ?>" class="block group">
                             <?php if (has_post_thumbnail()) : ?>
-                                <img class="block w-full h-64 mb-6 object-cover" 
+                                    <img loading="lazy" class="block w-full group-hover:opacity-80 transition-opacity duration-200 mb-6 object-cover <?php echo match($GLOBALS['wp_query']->current_post % 5) {
+                                        0 => 'h-64',
+                                        1 => 'h-72',
+                                        2 => 'h-80', 
+                                        3 => 'h-88',
+                                        4 => 'h-96'
+                                    }; ?>"
                                      src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" 
+                                     alt="<?php the_title(); ?>">
+                            <?php else: ?>
+                                    <img loading="lazy" class="block w-full mb-6 object-cover h-64"
+                                     src="<?php echo get_template_directory_uri(); ?>/dist/images/placeholder.jpg" 
                                      alt="<?php the_title(); ?>">
                             <?php endif; ?>
                             <div>
@@ -72,7 +82,7 @@
                                 <p class="text-gray-700 mb-4 text-sm">
                                     <?php echo wp_trim_words(get_the_excerpt(), 20); ?>
                                 </p>
-                                <div class="text-2xl font-bold text-teal-900">
+                                <div class="text-md font-bold text-teal-900">
                                     <?php echo $product->get_price_html(); ?>
                                 </div>
                             </div>
